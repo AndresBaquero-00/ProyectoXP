c69f20e4d80a6d0849af7c7455613d0c76c89fc8
AndresBaquero-00 <andres.baquero6@gmail.com>
2021-09-27 09:27:45 -0500

    añadidos metodos de filtracion por departamento, cultivo y area sembrada

--- project/filters.py
+++ project/filters.py
@@ -3,4 +3,25 @@ class Filter:
     def __init__(self, data: list) -> None:
         self.data = data
     
+    def get_departments(self) -> list:
+        rows: list = []
+        for dato in self.data:
+            if rows.count(dato['departamento']) == 0:
+                rows.append(dato['departamento'])
+        return rows
+
+    def get_cultivos(self) -> list:
+        rows: list = []
+        for dato in self.data:
+            if rows.count(dato['cultivo']) == 0:
+                rows.append(dato['cultivo'])
+        return rows
+
+    def get_area_sembrada_by_department(self, department: str) -> list:
+        rows: list = []
+        for dato in self.data:
+            if dato['departamento'] == department:
+                rows.append(dato['area_sembrada_ha'])
+
+        return rows
     
