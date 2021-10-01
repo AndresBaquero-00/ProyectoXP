f7682777cdb3564c788507943248af3aeaa219f4
AndresBaquero-00 <andres.baquero6@gmail.com>
2021-10-01 13:10:17 -0500

    Metodo de almacenar las imagenes en formato binario mejorado

--- project/charts.py
+++ project/charts.py
@@ -1,31 +1,35 @@
-import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import numpy as np
 import base64, io
 
 class Charts:
-    def horizontal_bar(self, x: list, y: list, xlabel: str):
-        fig, ax = plt.subplots()
+    figure: Figure
+    
+    def horizontal_bar(self, x: list, y: list, xlabel: str) -> str:
+        self.figure = Figure()
+        ax = self.figure.subplots()
         y_pos = np.arange(len(x))
         ax.barh(y_pos, y, align = 'center')
         ax.set_yticks(y_pos)
         ax.set_yticklabels(x)
         ax.invert_yaxis()
         ax.set_xlabel(xlabel)
-        plt.show()
-        # return self.save_chart()
 
-    def pie_chart(self, labels: list, sizes: list, title: str):
-        fig, ax = plt.subplots()
+        return self.save_fig()
+
+    def pie_chart(self, labels: list, sizes: list, title: str) -> str:
+        self.figure = Figure()
+        ax = self.figure.subplots()
         ax.pie(sizes, None, labels = labels, autopct = '%1.1f%%', startangle = 90)
         ax.set(aspect = 'equal', title = title)
-        plt.show()
-        # return self.save_chart()
-
-    def save_chart(self):
+        # plt.show()
+        return self.save_fig()
+        
+    def save_fig(self) -> str:
         img = io.BytesIO()
-        plt.savefig(img, format = 'png')
-        img.seek(0)
-        plot = base64.b64encode(img.getvalue()).decode()
-        return plot
+        self.figure.savefig(img, format = 'png')
+        plot = base64.b64encode(img.getbuffer()).decode('ascii')
+        return f"<img src='data:image/png;base64,{plot}'/>"
+        
         
 
